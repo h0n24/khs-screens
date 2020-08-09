a727e97b4525308e8bf2eb8f0a71d7586eb364ef
Jan Šablatura <h0n24.cz@gmail.com>
2020-08-09 19:30:45 +0200

    OCR multi test

--- generate/test.js
+++ generate/test.js
@@ -36,7 +36,7 @@
 // });
 
 const sharp = require('sharp');
-const Tesseract = require('tesseract.js');
+// const Tesseract = require('tesseract.js');
 
 const OCRpozice = {
     "1-c": [116, 282, 55, 36],
@@ -59,31 +59,90 @@ const OCRpozice = {
     "7-h": [343, 588, 55, 36]
 }
 
+let OCRurl = [];
+
 const okraj = true;
 const okraj1 = okraj ? 1 : 0;
 const okraj2 = okraj ? -2 : 0;
 
-// for (const pozice in OCRpozice) {
-//     if (OCRpozice.hasOwnProperty(pozice)) {
-//         const crop = OCRpozice[pozice];
-//         const saveToFile = `ocr/${pozice}.png`;
+for (const pozice in OCRpozice) {
+    if (OCRpozice.hasOwnProperty(pozice)) {
+        const crop = OCRpozice[pozice];
+        const saveToFile = `ocr/${pozice}.png`;
 
-//         // treshold 180 zbarví pixely pod 180 do černé, 
-//         // zbytek bílá
-//         sharp('out/10-khsplzen-ocr.png')
-//             .extract({
-//                 left: crop[0] + okraj1,
-//                 top: crop[1] + okraj1,
-//                 width: crop[2] + okraj2,
-//                 height: crop[3] + okraj2
-//             })
-//             .threshold(180)
-//             .toFile(saveToFile, function (err) {
-//                 if (err) console.log(err);
-//                 // getTextFromImage(saveToFile).then(console.log)
-//             })
-//     }
-// }
+        // treshold 180 zbarví pixely pod 180 do černé, 
+        // zbytek bílá
+        sharp('out/10-khsplzen-ocr.png')
+            .extract({
+                left: crop[0] + okraj1,
+                top: crop[1] + okraj1,
+                width: crop[2] + okraj2,
+                height: crop[3] + okraj2
+            })
+            .threshold(180)
+            .toFile(saveToFile, function (err) {
+                if (err) console.log(err);
+                OCRurl.push(saveToFile);
+                // OCRtesseract(saveToFile);
+                // getTextFromImage(saveToFile).then(console.log)
+
+                // console.log(OCRurl);
+            })
+    }
+}
+
+
+
+// import { createWorker } from 'tesseract.js';
+const { createWorker } = require('tesseract.js');
+
+const worker = createWorker({
+  logger: m => console.log(m)
+});
+
+(async () => {
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  const { data: { text } } = await worker.recognize(OCRurl[0]);
+  console.log(text);
+  await worker.terminate();
+})();
+
+(async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text2 } } = await worker.recognize(OCRurl[1]);
+    console.log(text2);
+    await worker.terminate();
+  })();
+
+
+// const { createWorker, createScheduler } = require('tesseract.js');
+
+// const scheduler = createScheduler();
+// const worker1 = createWorker();
+// const worker2 = createWorker();
+
+// (async () => {
+//   await worker1.load();
+//   await worker2.load();
+//   await worker1.loadLanguage('eng');
+//   await worker2.loadLanguage('eng');
+//   await worker1.initialize('eng');
+//   await worker2.initialize('eng');
+//   scheduler.addWorker(worker1);
+//   scheduler.addWorker(worker2);
+//   /** Add 10 recognition jobs */
+//   const results = await Promise.all(OCRurl.map(obj => {
+//       console.log(obj);
+//     scheduler.addJob('recognize', obj)
+//   }))
+
+//   console.log(results);
+//   await scheduler.terminate(); // It also terminates all workers.
+// })();
 
 // -----------------------------------------------------------------------
 
@@ -135,44 +194,44 @@ const okraj2 = okraj ? -2 : 0;
 
 // -------------------------------
 
-sharp('out/10-khsplzen-ocr.png')
-.threshold(180)
-.toFile('out/10-khsplzen-ocr-bw.png', function (err) {
-    if (err) console.log(err);
-})
+// sharp('out/10-khsplzen-ocr.png')
+// .threshold(180)
+// .toFile('out/10-khsplzen-ocr-bw.png', function (err) {
+//     if (err) console.log(err);
+// })
 
-const { createWorker, createScheduler } = require('tesseract.js');
+// const { createWorker, createScheduler } = require('tesseract.js');
 
-const scheduler = createScheduler();
-const worker1 = createWorker();
-const worker2 = createWorker();
-const rectangles = [
-  {
-    left: 116,
-    top: 282,
-    width: 55,
-    height: 36,
-  },
-  {
-    left: 172,
-    top: 282,
-    width: 55,
-    height: 36,
-  },
-];
+// const scheduler = createScheduler();
+// const worker1 = createWorker();
+// const worker2 = createWorker();
+// const rectangles = [
+//   {
+//     left: 116,
+//     top: 282,
+//     width: 55,
+//     height: 36,
+//   },
+//   {
+//     left: 172,
+//     top: 282,
+//     width: 55,
+//     height: 36,
+//   },
+// ];
 
-(async () => {
-  await worker1.load();
-  await worker2.load();
-  await worker1.loadLanguage('eng');
-  await worker2.loadLanguage('eng');
-  await worker1.initialize('eng');
-  await worker2.initialize('eng');
-  scheduler.addWorker(worker1);
-  scheduler.addWorker(worker2);
-  const results = await Promise.all(rectangles.map((rectangle) => (
-    scheduler.addJob('recognize', 'out/10-khsplzen-ocr-bw.png', { rectangle })
-  )));
-  console.log(results.map(r => r.data.text));
-  await scheduler.terminate();
-})();
+// (async () => {
+//   await worker1.load();
+//   await worker2.load();
+//   await worker1.loadLanguage('eng');
+//   await worker2.loadLanguage('eng');
+//   await worker1.initialize('eng');
+//   await worker2.initialize('eng');
+//   scheduler.addWorker(worker1);
+//   scheduler.addWorker(worker2);
+//   const results = await Promise.all(rectangles.map((rectangle) => (
+//     scheduler.addJob('recognize', 'out/10-khsplzen-ocr-bw.png', { rectangle })
+//   )));
+//   console.log(results.map(r => r.data.text));
+//   await scheduler.terminate();
+// })();
