c696a71dd607db08d77e201faf9860f0c9813231
Jan Šablatura <h0n24.cz@gmail.com>
2020-08-14 20:14:44 +0200

    fallback back from testing

--- generate/download.js
+++ generate/download.js
@@ -18,20 +18,20 @@ fs.writeFileSync('out/time.json', "[]");
 // okres, pozitivni, vyleceni, umrti, aktivni, obyvatel
 
 // jednotlivé scripty pro khs
-// require("./khs/01-khscb")();
-// require("./khs/02-khsbrno")();
-// require("./khs/03-khskv")();
-// require("./khs/04-khsjih")();
-// require("./khs/05-khshk")();
-// require("./khs/06-khslbc")();
-// require("./khs/07-khsova")();
-// require("./khs/08-khsolc")();
-// require("./khs/09-khspce")();
+require("./khs/01-khscb")();
+require("./khs/02-khsbrno")();
+require("./khs/03-khskv")();
+require("./khs/04-khsjih")();
+require("./khs/05-khshk")();
+require("./khs/06-khslbc")();
+require("./khs/07-khsova")();
+require("./khs/08-khsolc")();
+require("./khs/09-khspce")();
 require("./khs/10-khsplzen")();
-// require("./khs/11-hygpraha")();
-// require("./khs/12-khsstc")();
-// require("./khs/13-khsusti")();
-// require("./khs/14-khszlin")();
+require("./khs/11-hygpraha")();
+require("./khs/12-khsstc")();
+require("./khs/13-khsusti")();
+require("./khs/14-khszlin")();
 
 // todo
 // exposeFunction pro clean
